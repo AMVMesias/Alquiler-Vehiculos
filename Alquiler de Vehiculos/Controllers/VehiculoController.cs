b34c83115c740019f2c77b935e66fee63c800c12
Mariscal Oña Mesias Orlando <mesiasv2@gmail.com>
2025-03-03 02:56:10 -0500

    Conexion establecida - Vehiculos 1.0

---
+++ Alquiler de Vehiculos/Controllers/VehiculoController.cs
@@ -0,0 +1,102 @@
+﻿using CapaEntidad;
+using CapaNegocio;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Alquiler.Controllers
+{
+    public class VehiculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+
+        public IActionResult Vehiculos()
+        {
+            return View();
+        }
+
+        // Métodos para devolver datos directamente
+        public List<VehiculoCLS> ListarVehiculos()
+        {
+            VehiculoBL obj = new VehiculoBL();
+            return obj.ListarVehiculos();
+        }
+
+        public List<VehiculoCLS> FiltrarVehiculos(string marca, string modelo)
+        {
+            VehiculoBL obj = new VehiculoBL();
+            return obj.FiltrarVehiculos(marca, modelo);
+        }
+
+        public VehiculoCLS RecuperarVehiculo(int idVehiculo)
+        {
+            // Supongo que necesitarás implementar este método en tu capa de negocio
+            VehiculoBL obj = new VehiculoBL();
+            List<VehiculoCLS> lista = obj.ListarVehiculos();
+            return lista.Find(v => v.Id == idVehiculo);
+        }
+
+        public int GuardarDatos(VehiculoCLS vehiculo)
+        {
+            VehiculoBL obj = new VehiculoBL();
+            return obj.GuardarDatosVehiculo(vehiculo);
+        }
+
+        public string EliminarVehiculo(int id)
+        {
+            VehiculoBL obj = new VehiculoBL();
+            int rpta = obj.EliminarVehiculo(id);
+            return rpta.ToString();
+        }
+
+        // Vistas para operaciones CRUD (sin parámetros)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        // Ejemplos de métodos adicionales útiles
+        public List<string> ListarMarcas()
+        {
+            // Implementar si necesitas un método para obtener solo las marcas distintas
+            VehiculoBL obj = new VehiculoBL();
+            List<VehiculoCLS> vehiculos = obj.ListarVehiculos();
+
+            // Extraer marcas únicas
+            HashSet<string> marcasUnicas = new HashSet<string>();
+            foreach (var vehiculo in vehiculos)
+            {
+                marcasUnicas.Add(vehiculo.Marca);
+            }
+
+            return new List<string>(marcasUnicas);
+        }
+
+        public string ObtenerVersion()
+        {
+            return "Versión 1.0";
+        }
+    }
+}
--- CapaNegocio/VehiculoBL.cs
+++ CapaNegocio/VehiculoBL.cs
@@ -1,8 +1,32 @@
-﻿namespace CapaNegocio
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+
+namespace CapaNegocio
 {
     public class VehiculoBL
     {
-        /*Estos comentarios son para ver si jala el push y el pulll */
+        private VehiculoDAL vehiculoDAL = new VehiculoDAL();
 
+        public List<VehiculoCLS> ListarVehiculos()
+        {
+            return vehiculoDAL.ListarVehiculos();
+        }
+
+        public List<VehiculoCLS> FiltrarVehiculos(string marca, string modelo)
+        {
+            return vehiculoDAL.FiltrarVehiculos(marca, modelo);
+        }
+
+        public int GuardarDatosVehiculo(VehiculoCLS objVehiculo)
+        {
+            return vehiculoDAL.GuardarDatosVehiculo(objVehiculo);
+        }
+
+        public int EliminarVehiculo(int id)
+        {
+            return vehiculoDAL.EliminarVehiculo(id);
+        }
     }
 }
